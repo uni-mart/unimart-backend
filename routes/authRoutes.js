f8d718f95bfae5e1a0881826580030cb0788fa63
tomiwadotJS <victorgbayedev@gmail.com>
2023-04-13 03:51:15 +0100

    login and logout controller and route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { register, login, logout } = require('../controllers/authController');
 
 router.post('/register', register);
-// router.post('/login', login);
-// router.get('/logout', logout);
+router.post('/login', login);
+router.get('/logout', logout);
 
 module.exports = router;
